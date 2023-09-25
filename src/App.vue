546396ebcf3bbe53614a21184329e2207f7312a7
unknown <SENA@SGLDFPCATSD262.FormacionSena.RED>
2023-09-25 14:41:39 -0500

    Actuañizacion 25/09/2023

--- src/App.vue
+++ src/App.vue
@@ -1,188 +1,28 @@
-<script setup>
-/* import {ref} from "vue" */
-import axios from "axios";
-import { ref, onMounted } from "vue";
-
-const img = ref();
-const name = ref();
-const Id = ref();
-const StatHp = ref();
-const Attack = ref();
-const Defense = ref();
-const SpecialAttack = ref();
-const SpecialDefense = ref();
-const Speed = ref();
-const Weight = ref();
-const Height = ref();
-const tipoPk = ref([]);
-
-const links1 = ref([
-  "https://pokeapi.co/api/v2/pokemon/1",
-  "https://pokeapi.co/api/v2/pokemon/2",
-  "https://pokeapi.co/api/v2/pokemon/3",
-  "https://pokeapi.co/api/v2/pokemon/4",
-  "https://pokeapi.co/api/v2/pokemon/5",
-  "https://pokeapi.co/api/v2/pokemon/6",
-  "https://pokeapi.co/api/v2/pokemon/7",
-  "https://pokeapi.co/api/v2/pokemon/8",
-  "https://pokeapi.co/api/v2/pokemon/9",
-  "https://pokeapi.co/api/v2/pokemon/10",
-  "https://pokeapi.co/api/v2/pokemon/11",
-  "https://pokeapi.co/api/v2/pokemon/12",
-  "https://pokeapi.co/api/v2/pokemon/13",
-  "https://pokeapi.co/api/v2/pokemon/14",
-  "https://pokeapi.co/api/v2/pokemon/15",
-  "https://pokeapi.co/api/v2/pokemon/16",
-  "https://pokeapi.co/api/v2/pokemon/17",
-  "https://pokeapi.co/api/v2/pokemon/18",
-  "https://pokeapi.co/api/v2/pokemon/19",
-  "https://pokeapi.co/api/v2/pokemon/20",
-  "https://pokeapi.co/api/v2/pokemon/21",
-  "https://pokeapi.co/api/v2/pokemon/22",
-  "https://pokeapi.co/api/v2/pokemon/23",
-  "https://pokeapi.co/api/v2/pokemon/24",
-  "https://pokeapi.co/api/v2/pokemon/25",
-  "https://pokeapi.co/api/v2/pokemon/26",
-  "https://pokeapi.co/api/v2/pokemon/27",
-  "https://pokeapi.co/api/v2/pokemon/28",
-  "https://pokeapi.co/api/v2/pokemon/29",
-  "https://pokeapi.co/api/v2/pokemon/30",
-  "https://pokeapi.co/api/v2/pokemon/31",
-  "https://pokeapi.co/api/v2/pokemon/32",
-  "https://pokeapi.co/api/v2/pokemon/33",
-  "https://pokeapi.co/api/v2/pokemon/34",
-  "https://pokeapi.co/api/v2/pokemon/35",
-  "https://pokeapi.co/api/v2/pokemon/36",
-  "https://pokeapi.co/api/v2/pokemon/37",
-  "https://pokeapi.co/api/v2/pokemon/38",
-  "https://pokeapi.co/api/v2/pokemon/39",
-  "https://pokeapi.co/api/v2/pokemon/40",
-  "https://pokeapi.co/api/v2/pokemon/41",
-  "https://pokeapi.co/api/v2/pokemon/42",
-  "https://pokeapi.co/api/v2/pokemon/43",
-  "https://pokeapi.co/api/v2/pokemon/44",
-  "https://pokeapi.co/api/v2/pokemon/45",
-  "https://pokeapi.co/api/v2/pokemon/46",
-  "https://pokeapi.co/api/v2/pokemon/47",
-  "https://pokeapi.co/api/v2/pokemon/48",
-  "https://pokeapi.co/api/v2/pokemon/49",
-  "https://pokeapi.co/api/v2/pokemon/50",
-]);
-const pokemonData = ref([]);
-const Mostrar = ref(false);
-const MostrarMain = ref(true);
-async function obtenerurlsPokemon() {
- 
-  pokemonData.value = [];
-  for (const link of links1.value) {
-    let data = await axios.get(link);
-    pokemonData.value.push({
-      img: data.data.sprites.other["official-artwork"].front_default,
-      name: data.data.name,
-      Id: data.data.id,
-      StatHp: data.data.stats[0].base_stat,
-      Attack: data.data.stats[1].base_stat,
-      Defense: data.data.stats[2].base_stat,
-      SpecialAttack: data.data.stats[3].base_stat,
-      SpecialDefense: data.data.stats[4].base_stat,
-      Speed: data.data.stats[5].base_stat,
-      Weight: data.data.weight,
-      Height: data.data.height,
-      tipoPk: data.data.types.map((element) => element.type.name),
-    });
-  }
-  /* Mostrar.value = true */
-}
-async function obtenerDetallesPokemon() {
-  
-  pokemonData.value = [];
-  for (const link of links1.value) {
-    let data = await axios.get(link);
-    pokemonData.value.push({
-      img: data.data.sprites.other["official-artwork"].front_default,
-      name: data.data.name,
-      Id: data.data.id,
-      StatHp: data.data.stats[0].base_stat,
-      Attack: data.data.stats[1].base_stat,
-      Defense: data.data.stats[2].base_stat,
-      SpecialAttack: data.data.stats[3].base_stat,
-      SpecialDefense: data.data.stats[4].base_stat,
-      Speed: data.data.stats[5].base_stat,
-      Weight: data.data.weight,
-      Height: data.data.height,
-      tipoPk: data.data.types.map((element) => element.type.name),
-    });
-  }
-  Mostrar.value = true
-  MostrarMain.value = false
-}
-function getColorByType(tipo) {
-  if (tipo === "fire") {
-    return "#FDDFDF";
-  } else if (tipo === "grass") {
-    return "#DEFDE0";
-  } else if (tipo === "grass" && tipo === "poison") {
-    return "#DEFDE0";
-  }else if (tipo === "electric") {
-    return "#FCF7DE";
-  } else if (tipo === "water") {
-    return "#DEF3FD";
-  } else if (tipo === "ground") {
-    return "#f4e7da";
-  } else if (tipo === "rock") {
-    return "#d5d5d4";
-  } else if (tipo === "fairy") {
-    return "#fceaff";
-  } else if (tipo === "poison") {
-    return "#E1D1FF";
-  } else if (tipo === "bug") {
-    return "#E6FFC9";
-  } else if (tipo === "dragon") {
-    return "#97b3e6";
-  } else if (tipo === "psychic") {
-    return "#eaeda1";
-  } else if (tipo === "flying") {
-    return "#F5F5F5";
-  } else if (tipo === "fighting") {
-    return "#E6E0D4";
-  } else {
-    return "white"; // Si el tipo no se encuentra, establece el color en blanco
-  }
-} // Si el tipo no se encuentra, se establece en blanco
-
-
-
-
-
-onMounted(() => {
-  obtenerurlsPokemon(); // Llama a la función cuando la página se carga
-});
-</script>
-
 <template>
   <div class="body">
     <div class="nav">
-      <h1>Pokedex</h1>
+      <button class="PokeRes" @click="Volver()">Pokedex</button>
     </div>
 
     <div class="main" v-if="MostrarMain">
-      <buuton v-for="(pokemon, index) in pokemonData" :key="index"
-      :style="{ backgroundColor: getColorByType(pokemon.tipoPk[0])}"
-      class="Tarjeta"
-      @click="obtenerDetallesPokemon()">
+      <button v-for="(pokemon, index) in pokemonData" :key="index"
+        :style="{ backgroundColor: getColorByType(pokemon.tipo_pk[0]) }" 
+        @click="obtenerDetallesPokemon(pokemon)"
+        class="Tarjeta">
+
         <div class="containerImgTarjeta">
           <img :src="pokemon.img" alt="" class="imgTarjeta" />
         </div>
         <div class="containerDatosTarjeta">
-          <p style="margin: 0;" class="IdTarjeta">#{{ pokemon.Id }}</p>
-          <p style="margin: 0;">{{ pokemon.name }}</p>
-          <p style="margin: 0;">Tipo: {{ pokemon.tipoPk[0] }} {{ pokemon.tipoPk[1] }} </p>
+          <p style="margin: 0;" class="IdTarjeta">#{{ pokemon.numero }}</p>
+          <p style="margin: 0;">{{ pokemon.nombre }}</p>
+          <p style="margin: 0;" v-if="MostrarMain">{{ pokemon.tipo_pk[0] }} {{ pokemon.tipo_pk[1] }} </p>
         </div>
-      </buuton>
+      </button>
     </div>
 
-    <div class="Detalles" v-if="Mostrar">
-      <div class="funcion" v-if="Mostrar">
+    <div class="Detalles" v-if="Mostrar"  :style="{ backgroundColor: getColorByType() }">
+      <div class="funcion"  >
         <div class="funcionPart1">
           <div class="nameContainer">
             <h2 class="name">{{ name }}</h2>
@@ -192,26 +32,27 @@ onMounted(() => {
             <h2 class="Id">#{{ Id }}</h2>
             <img :src="img" class="img" />
           </div>
-          <div class="Types">
-            <h2 v-for="(item, index) in tipoPk" :key="index" class="types">
+          <div class="Types" >
+            <h2 v-for="(item, index) in tipoPk" :key="index" class="types"  
+            :style="{ backgroundColor: getColorByTypeDetalle(item) }">
               {{ item }}
             </h2>
           </div>
         </div>
         <div>
-          <div class="datos" v-for="(i, index) in pokemonData" :key="index">
-            <img :src="img" class="img" />
-            <h2 class="Id">#{{ i.Id }}</h2>
-          <h2>{{ i.name }}</h2>
-          <h2>Tipo: {{ i.tipoPk[0] }} {{ i.tipoPk[1] }} </h2>
-            <h2>HP: {{ i.StatHp }}</h2>
-            <h2>Attack: {{ i.Attack }}</h2>
-            <h2>Defense: {{ i.Defense }}</h2>
-            <h2>SpecialAttack: {{ i.SpecialAttack }}</h2>
-            <h2>SpecialDefense: {{ i.SpecialDefense }}</h2>
-            <h2>Speed: {{ i.Speed }}</h2>
-            <h2>Weight: {{ i.Weight }}KG</h2>
-            <h2>Height: {{ i.Height }}</h2>
+          <div class="datos" >
+            
+            
+            <h2>{{ nombre }}</h2>
+
+            <h2>HP: {{ StatHp }}</h2>
+            <h2>Attack: {{ Attack }}</h2>
+            <h2>Defense: {{ Defense }}</h2>
+            <h2>SpecialAttack: {{ SpecialAttack }}</h2>
+            <h2>SpecialDefense: {{ SpecialDefense }}</h2>
+            <h2>Speed: {{ Speed }}</h2>
+            <h2>Weight: {{ Weight }}KG</h2>
+            <h2>Height: {{ Height }}</h2>
           </div>
         </div>
       </div>
@@ -219,6 +60,162 @@ onMounted(() => {
   </div>
 </template>
 
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+let img = ref('');
+let name = ref('');
+let Id = ref('');
+let StatHp = ref('');
+let Attack = ref('');
+let Defense = ref('');
+let SpecialAttack = ref('');
+let SpecialDefense = ref('');
+let Speed = ref('');
+let Weight = ref('');
+let Height = ref('');
+let tipoPk = ref([]);
+
+let pokemonData = ref([]);
+let Mostrar = ref(false);
+let MostrarMain = ref(true);
+
+async function obtenerDetallesPokemon(pokemon) {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.numero}`);
+    const data = response.data;
+
+    img.value = data.sprites.other["official-artwork"].front_default;
+    name.value = data.name;
+    Id.value = data.id;
+    StatHp.value = data.stats[0].base_stat;
+    Attack.value = data.stats[1].base_stat;
+    Defense.value = data.stats[2].base_stat;
+    SpecialAttack.value = data.stats[3].base_stat;
+    SpecialDefense.value = data.stats[4].base_stat;
+    Speed.value = data.stats[5].base_stat;
+    Weight.value = data.weight;
+    Height.value = data.height;
+    tipoPk.value = data.types.map((element) => element.type.name);
+
+    Mostrar.value = true;
+    MostrarMain.value = false;
+  } catch (error) {
+    console.error("Error al obtener detalles del Pokémon:", error);
+  }
+}
+
+function Volver() {
+  img.value = '';
+  Id.value = '';
+  name.value = '';
+  StatHp.value = '';
+  Attack.value = '';
+  Defense.value = '';
+  SpecialAttack.value = '';
+  SpecialDefense.value = '';
+  Speed.value = '';
+  tipoPk.value = [];
+  Mostrar.value = false;
+  MostrarMain.value = true;
+}
+
+function getColorByType(tipo) {
+  switch (tipo) {
+    case "fire":
+      return "#FDDFDF";
+    case "grass":
+      return "#DEFDE0";
+    case "electric":
+      return "#FCF7DE";
+    case "water":
+      return "#DEF3FD";
+    case "ground":
+      return "#f4e7da";
+    case "rock":
+      return "#d5d5d4";
+    case "fairy":
+      return "#fceaff";
+    case "poison":
+      return "#E1D1FF";
+    case "bug":
+      return "#E6FFC9";
+    case "dragon":
+      return "#97b3e6";
+    case "psychic":
+      return "#eaeda1";
+    case "flying":
+      return "#F5F5F5";
+    case "fighting":
+      return "#E6E0D4";
+    default:
+      return "white";
+  }
+}
+
+function getColorByTypeDetalle(tipo) {
+  switch (tipo) {
+    case "fire":
+      return "#FF2D00";
+    case "grass":
+      return "#009121";
+    case "electric":
+      return "#8D8F00";
+    case "water":
+      return "#00B7FF";
+    case "ground":
+      return "#635200";
+    case "rock":
+      return "#525252";
+    case "fairy":
+      return "#ED7FFF";
+    case "poison":
+      return "#B600C8";
+    case "bug":
+      return "#87C800";
+    case "dragon":
+      return "#AC0000";
+    case "psychic":
+      return "#008F88";
+    case "flying":
+      return "#6D6984";
+    case "fighting":
+      return "#663030";
+    default:
+      return "3A6630";
+      
+  }
+}
+
+onMounted(() => {
+  obtenerUrlsPokemon(); // Llama a la función cuando la página se carga
+});
+
+async function obtenerUrlsPokemon() {
+  try {
+    for (let i = 1; i <= 50; i++) {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const data = response.data;
+
+      pokemonData.value.push({
+        img: data.sprites.other["official-artwork"].front_default,
+        numero: data.id,
+        nombre: data.name,
+        hp: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        defense: data.stats[2].base_stat,
+        specialattack: data.stats[3].base_stat,
+        specialdefense: data.stats[4].base_stat,
+        speed: data.stats[5].base_stat,
+        tipo_pk: data.types.map((element) => element.type.name),
+      });
+    }
+  } catch (error) {
+    console.error("Error al obtener la lista de Pokémon:", error);
+  }
+}
+</script>
 <style scoped>
 .body {
   display: flex;
@@ -243,27 +240,34 @@ onMounted(() => {
 .imgTarjeta {
   width: 100px;
 }
+
 .Tarjeta {
   display: flex;
   flex-direction: column;
   align-items: center;
- color: black;
+  color: black;
   border-radius: 15px;
   padding: 15px;
   gap: 15px;
   cursor: pointer;
+  width: 200px;
 }
-.containerDatosTarjeta{
+
+.containerDatosTarjeta {
   display: flex;
   flex-direction: column;
   align-items: center;
-  
+  font-size: 25px;
+  gap: 10px;
+  font-weight: 500;
 }
-.containerImgTarjeta{
+
+.containerImgTarjeta {
   padding: 15px;
   background-color: white;
   border-radius: 50%;
 }
+
 .botonPeticion {
   border-radius: 10px;
   padding: 15px;
@@ -285,12 +289,13 @@ onMounted(() => {
   margin: 0;
   display: flex;
   justify-content: center;
+  padding: 5px 0px;
 }
 
 .main {
   display: flex;
   justify-content: center;
-  padding: 0px 100px;
+  padding: 0px 200px;
   flex-wrap: wrap;
   gap: 85px;
 }
@@ -313,7 +318,14 @@ onMounted(() => {
   flex-direction: column;
   flex-wrap: wrap;
 }
-
+.PokeRes{
+  background-color: transparent;
+  border: none;
+  font-family: "Pa ver";
+  color: white;
+  font-size: 55px;
+  cursor: pointer;
+}
 .containerImg {
   display: flex;
   align-items: center;
@@ -331,7 +343,7 @@ onMounted(() => {
 }
 
 .types {
-  background-color: rgb(0, 89, 255);
+  
   padding: 10px;
   border-radius: 10px;
 }
@@ -341,15 +353,18 @@ onMounted(() => {
   color: rgb(0, 89, 255);
   margin: 0;
 }
-.IdTarjeta{
+
+.IdTarjeta {
   padding: 5px;
   background-color: rgba(0, 0, 0, 0.337);
   border-radius: 5px;
 }
+
 .name {
   font-size: 100px;
   margin: 0;
 }
+
 .funcionPart1 {
   display: flex;
   flex-direction: column;
